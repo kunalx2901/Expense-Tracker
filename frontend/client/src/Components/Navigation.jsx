b3736cb4b3245b6ab9c3d5de4c3689a1efb97431
kunalx2901 <kunal.sharma.29.06.2003@gmail.com>
2024-07-28 21:01:34 +0530

    frontend"1"

--- frontend/client/src/Components/Navigation.jsx
+++ frontend/client/src/Components/Navigation.jsx
@@ -1,16 +1,27 @@
 import { menuItems } from '../utils/menuItems'
-import avatar from "../images/3d-cartoon-character.jpg"
+import avatar from "../images/avatar.avif"
+import { signout } from '../utils/icons'
 
 function Navigation() {
   return (
-    <div>
-        <img className= "h-20 w-20 rounded-full"src={avatar} alt=""/>
-        <div>
+    <div className="h-[50vh] w-full overflow-hidden rounded-lg border-2 border-solid border-slate-400 lg:h-[100vh] lg:w-52 lg:flex-col lg:justify-center lg:items-center ">
+        <div className="flex jusitfy-center items-center gap-3 mt-20 ml-4 ">
+        <img className= "h-[70px] w-[70px] rounded-full"src={avatar} alt=""/>
+          <div className="flex-col justify-center items-center">
+              <h2 className="font-bold text-xl">Kunal</h2>
+              <span className="font-black">My Money</span>
+          </div>
+        </div>
+        <div className="mt-10 ml-5">
           <ul>
             {menuItems.map((items)=>{
-               return <li key={items.id}> {items.icon} <span>{items.title}</span></li>
+               return <li className="mb-3" key={items.id}> {items.icon} <span>{items.title}</span></li>
             })}
           </ul>
+
+          <div>
+           {signout} Sign out
+          </div>
         </div>
 
     </div>
--- frontend/client/src/utils/icons.jsx
+++ frontend/client/src/utils/icons.jsx
@@ -2,3 +2,4 @@ export const dashboard = <i className="fa-solid fa-chart-line"></i>
 export const income = <i className="fa-solid fa-sack-dollar"></i>
 export const viewTransaction = <i className="fa-solid fa-money-bill-transfer"></i>
 export const expense = <i className="fa-solid fa-receipt"></i>
+export const signout = <i className="fa-solid fa-arrow-right-from-bracket"></i>
